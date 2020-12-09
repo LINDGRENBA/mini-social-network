9b1c17cf66e950cf39b93f600851b435463378e7
Brittany Lindgren <brittdevelopsit@gmail.com>
2020-12-09 10:29:32 -0800

    Pass test to remove an edge between two nodes.

--- src/graph.js
+++ src/graph.js
@@ -30,4 +30,9 @@ export default class Graph {
     return false;
   }
 
+  removeEdge(node1, node2) {
+    this.adjacencyList.get(node1).delete(node2);
+    this.adjacencyList.get(node2).delete(node1);
+  }
+
 }
